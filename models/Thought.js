9306974ecae48bfd890c195a7d9adddb120f02a3
Christian Smith <christiansmith@Christians-MBP.lan>
2023-05-27 13:09:13 -0400

    setup Thought.js model file

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAt) => createdAt.toISOString()
+        },
+        username: {
+            type: Schema.Types.ObjectId,
+            ref: 'user',
+            required: true
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+let reactionCunt = thoughtSchema.virtual('reactions').get(function() {
+    return this.reactions.length
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
