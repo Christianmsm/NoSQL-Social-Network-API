3992869ea17dc0b925f173ddcfc58c646b778087
Christian Smith <christiansmith@Christians-MBP.lan>
2023-05-27 20:35:38 -0400

    created data & seeds files + setup data file

---
+++ utils/data.js
@@ -0,0 +1,67 @@
+const { Types } = require('mongoose');
+
+const userData = [
+    {
+      username: 'Jim Hopper',
+      email: 'jimhop@email.com',
+      thoughts: [],
+      friends: []  
+    },
+    {
+        username: 'Nancy Wheeler',
+        email: 'nancydrew@email.com',
+        thoughts: [],
+        friends: []  
+    },
+    {
+        username: 'Mike Wheeler',
+        email: 'mikewheeler@email.com',
+        thoughts: [],
+        friends: []  
+    },
+    {
+        username: 'Steve Henderson',
+        email: 'stevehenderson@email.com',
+        thoughts: [],
+        friends: []  
+    },
+    {
+        username: 'Dustin Henderson',
+        email: 'dustin@email.com',
+        thoughts: [],
+        friends: []  
+    },
+    {
+        username: 'Lucas Sinclair',
+        email: 'lucassinclair@email.com',
+        thoughts: [],
+        friends: []  
+    },
+    {
+        username: 'El Hopper',
+        email: 'eleven@email.com',
+        thoughts: [],
+        friends: []  
+    },
+    {
+        username: 'Will Buyers',
+        email: 'willupsidedown@email.com',
+        thoughts: [],
+        friends: []  
+    },
+    {
+        username: 'Joyce Byers',
+        email: 'joyce@email.com',
+        thoughts: [],
+        friends: []  
+    },
+    {
+        username: 'Jonathan Byers',
+        email: 'jonathan@email.com',
+        thoughts: [],
+        friends: []  
+    },
+
+]
+
+module.exports = { userData };
